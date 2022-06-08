8e63b87b23979a79e997b068d99b587366997687
Ba <duongvanba.com@gmail.com>
2022-06-08 21:14:43 +0700

    Update

--- src/decorators/WhenMicroserviceReady.ts
+++ src/decorators/WhenMicroserviceReady.ts
@@ -1,6 +1,8 @@
 import { SubcribeTopicOptions } from "../types";
 import { DecoratorBuilder } from "../helpers/DecoratorBuilder";
 
-export const [WhenMicroserviceReady, listenReadyHooks, activeWhenReadyHooks] = DecoratorBuilder.createPropertyDecorator<SubcribeTopicOptions>(async function () {
-
+export const [WhenMicroserviceReady, listenReadyHooks, activeWhenReadyHooks] = DecoratorBuilder.createPropertyDecorator<SubcribeTopicOptions>(async function ({
+    method
+}) {
+    await this[method]()
 }) 
